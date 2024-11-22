0d54c1cc909b5f1300aa6daeb3ed23190ebf4602
jonasbondesson84 <jonasbondesson@yahoo.com>
2024-11-22 14:13:34 +0100

    added PostNote

---
+++ functions/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+
+const validateToken = {
+    before : async (request) => {
+        try {
+            const token = request.event.headers.authorization.replace('Bearer ', '');    
+
+            if (!token) throw new Error();
+
+            const data = jwt.verify(token, process.env.JWT_SECRET);
+
+            request.event.id = data.id;
+            request.event.username = data.username;
+
+            return request.response;
+
+        } catch (error) {
+            
+            request.event.error = '401';
+
+            return request.response;
+
+        }
+        
+
+    }
+
+
+
+}
+
+module.exports = {validateToken};
