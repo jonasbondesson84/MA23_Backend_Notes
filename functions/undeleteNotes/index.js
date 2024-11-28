32ede5eb6f21cf00ef41c0f0faf386cbe13ee230
jonasbondesson84 <jonasbondesson@yahoo.com>
2024-11-28 09:45:19 +0100

    added ttl

--- functions/undeleteNotes/index.js
+++ functions/undeleteNotes/index.js
@@ -9,7 +9,7 @@ import httpErrorHandler from '@middy/http-error-handler';
 import createHttpError from 'http-errors';
 const db = new AWS.DynamoDB.DocumentClient();
 
-const deleteNote =  async (event, context) => {
+const undeleteNote =  async (event, context) => {
    
     if(event.error && event.error === '401') {
         throw new createHttpError.Unauthorized('Invalid token');
@@ -18,7 +18,7 @@ const deleteNote =  async (event, context) => {
     
     const userID = event.id;
     const {noteID} = event.body;
-    const deleteAt = Math.floor(Date.now() / 1000) + 60;
+    const deleteAt = null;
   
     try {
         const params = {
@@ -34,23 +34,16 @@ const deleteNote =  async (event, context) => {
             ReturnValues: 'UPDATED_NEW'
           };
           const result = await db.update(params).promise();
-        // const params = {
-        //                 TableName: 'notes-db',
-        //                 Key: {
-        //                     id: noteID,
-
-        //                 }
-        //               };
-        // await db.delete(params).promise();
+       
         
-        return sendResponse(200, {success: true, message: "Item scheduled for deletion"});
+        return sendResponse(200, {success: true, message: "Item undeleted"});
     } catch (error) {
         throw new createHttpError.InternalServerError('Database error');
         // return sendResponse(500, {message: "Couldnt delete note.", error: error})
     }
 }
 
-const handler = middy(deleteNote)
+const handler = middy(undeleteNote)
     .use(httpJsonBodyParser())
     .use(validateToken)
     .use(validator({eventSchema}))
